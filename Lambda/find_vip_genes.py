edab90ca49f164b1685119bc32a48972ee97e42e
Dan Linenberg <dan.linenberg@appsflyer.com>
2017-07-23 00:04:23 +0300

    script edit

--- Lambda/find_vip_genes.py
+++ Lambda/find_vip_genes.py
@@ -8,8 +8,9 @@ from  __builtin__ import any as b_any
 # Helper class to convert a DynamoDB item to JSON.
 def lambda_handler(event, context):
     
-    id = event['id']
-    drug = event['Key']
+    id = str(event['id'])
+    drug = str(event['Key'])
+
     dynamodb = boto3.resource('dynamodb')
     table_vcf = dynamodb.Table("VCF")
     table_vip = dynamodb.Table("GenesVIP")
@@ -46,7 +47,8 @@ def lambda_handler(event, context):
 
     print(dict_similar)
     for key,value in dict_similar.iteritems():
-        if b_any(str(drug).lower() in x for x in str(value).lower()) or b_any(x in str(drug).lower() for x in str(value).lower()):
-            return key
+        for word in drug.split():
+            if b_any(str(word).lower() in x for x in str(value).lower()) or b_any(x in str(word).lower() for x in str(value).lower()):
+                return key
     
     return False
